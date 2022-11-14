30858c92ccc7bf71c206fdb43e1dbd01d904c802
kcw0331 <changwoo03@naver.com>
2022-11-14 23:30:41 +0900

    프로그래머스 120848번: 팩토리얼

---
+++ programmers-coding/pr-120848.py
@@ -0,0 +1,14 @@
+# 프로그래머스 120848번: 팩토리얼
+def solution(n):
+    result = 0
+    D = {}
+    for i in range(1,11):
+        N = 1
+        for j in range(1,i+1):
+            N *= j
+        D[i] = N    # 1부터 10까지 팩토리얼 구한것을 딕셔너리에 추가
+    for i in range(1,11):   
+        if n >= D[i]:  # 최대의 i값을 찾아준다.
+            print(i)
+            result = i
+    return result
