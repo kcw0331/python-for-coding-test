4772d4e3829acfd58e18e077b1155e7eb2d7912d
Changwoo Kim <kcw0331@cau.ac.kr>
2022-11-25 23:39:12 +0900

    백준 10814번: 나이순 정렬

---
+++ baekjoo/10814.py
@@ -0,0 +1,9 @@
+# 백준 10814번: 나이순 정렬
+n = int(input())
+member = []
+for _ in range(n):
+  age, name = input().split()
+  member.append([int(age), name])
+member.sort(key = lambda x : x[0])
+for i in member:
+  print(i[0], i[1])
