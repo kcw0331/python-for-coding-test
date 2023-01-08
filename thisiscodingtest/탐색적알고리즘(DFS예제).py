aeba79d1bf8d3d94955bc4792adfd46054beabfa
Changwoo Kim <kcw0331@cau.ac.kr>
2023-01-08 23:40:50 +0900

    2023년1월8일-탐색적알고리즘(DFS예제) 수정

--- thisiscodingtest/탐색적알고리즘(DFS예제).py
+++ thisiscodingtest/탐색적알고리즘(DFS예제).py
@@ -5,10 +5,9 @@ def dfs(graph, v, visited):
     visited[v] = True
     print(v, end=' ')
     # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
-    print(visited)
+    # print(visited)
     for i in graph[v]:
         if not visited[i]: # 만약 visited[i]가 False라면 재귀 실행
-            print(visited[i])
             dfs(graph, i, visited)
 
 # 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
