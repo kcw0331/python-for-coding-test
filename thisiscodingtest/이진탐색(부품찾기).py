08fe8b48f2f22ad12cad3c9e477c705d3c254edf
Changwoo Kim <kcw0331@cau.ac.kr>
2023-01-22 13:09:49 +0900

    2023년1월22일-이진탐색(부품찾기_이진탐색,계수정렬,집합자료형이용)

--- thisiscodingtest/이진탐색(부품찾기).py
+++ thisiscodingtest/이진탐색(부품찾기).py
@@ -1,6 +1,18 @@
 # 부품찾기
+
 """ 
-# 내가 문제 풀어본 방식
+입력예시
+ 5
+ 8 3 7 9 2
+ 3
+ 5 7 9
+
+출력예시
+ne yes yes
+ """
+
+""" 
+# 1. 내가 문제 풀어본 방식
 import sys
 n = int(input())
 array = list(sys.stdin.readline().rstrip().split()) # 값을 읽어오고 리스트로 바꾸어줌
@@ -19,7 +31,7 @@ for i in array_find:
 for j in result:
     print(j, end=" ")
  """
-
+""" 
 # 책에 있는 방법을 사용하여 문제풀이
 # 방법 1) 답안 예시(이진탐색)
 # 이진 탐색 소스코드 구현(반복문)
@@ -56,3 +68,46 @@ for i in x:
         print('yes', end=" ")
     else:
         print('no', end=" ")
+ """
+""" 
+# 방법 2) 답안 예시(계수정렬)
+# N(가게의 부품 개수)을 입력받기
+n = int(input())
+array = [0] * 1000001 # 리스트안에 0을 1000001개만큼 채워준다.
+
+# 가게에 있는 전체 부품 번호를 입력받아서 기록
+for i in input().split():
+    array[int(i)] = 1 # 전체 부품 번호의 자리에 1을 넣어줌
+
+# M(손님이 확인 요청한 부품 개수)을 입력받기
+m = int(input())
+# 손님이 확인 요청한 전체 부품 번호를 공백으로 구분하여 입력
+x = list(map(int, input().split()))
+
+# 손님이 확인 요청한 부품 번호를 하나씩 확인
+for i in x:
+    # 해당 부품이 존재하는지 확인
+    if array[i] == 1:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+ """
+
+# 집합 자료형 이용
+# N(가게의 부품 개수)을 입력받기
+n = int(input())
+# 가게에 있는 전체 부품 번호를 입력받아서 집합(set) 자료형에 기록
+array = set(map(int, input().split())) # set 집합으로 초기화를 시켜준다.(중복도 제거된다.)
+
+# M(손님이 확인 요청한 부품 개수)을 입력받기
+m = int(input())
+# 손님이 확인 요청한 전체 부품 번호를 공백으로 구분하여 입력
+x = list(map(int, input().split()))
+
+# 손님이 확인 요청한 부품 번호를 하나씩 확인
+for i in x:
+    # 해당 부품이 존재하는지 확인
+    if i in array:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
