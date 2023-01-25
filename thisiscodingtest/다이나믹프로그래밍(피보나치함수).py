eb754c855f654130a46287f2cf75968b87d84e53
Changwoo Kim <kcw0331@cau.ac.kr>
2023-01-25 23:22:33 +0900

    2023년1월25일-다이나믹프로그래밍(피보나치함수)

--- thisiscodingtest/다이나믹프로그래밍(피보나치함수).py
+++ thisiscodingtest/다이나믹프로그래밍(피보나치함수).py
@@ -1,6 +1,21 @@
+"""
 # 피보나치 함수
 def fibo(x):
     if x == 1 or x == 2:
         return 1
     return fibo(x - 1) + fibo(x - 2)
 print(fibo(4))
+"""
+# 호출되는 함수 확인
+d = [0] * 100
+
+def pibo(x):
+    print('f(' + str(x) + ')', end=' ')
+    if x == 1 or x == 2:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    d[x] = pibo(x - 1) + pibo(x - 2)
+    print(d)
+    return d[x]
+pibo(6)
