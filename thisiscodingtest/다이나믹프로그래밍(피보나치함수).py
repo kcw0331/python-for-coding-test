7f29a297f513f111550d6f9740918249ed836de2
Changwoo Kim <kcw0331@cau.ac.kr>
2023-01-26 22:52:57 +0900

    2023년1월26일-다이나믹프로그래밍(피보나치함수(bottom-up))

--- thisiscodingtest/다이나믹프로그래밍(피보나치함수).py
+++ thisiscodingtest/다이나믹프로그래밍(피보나치함수).py
@@ -6,7 +6,9 @@ def fibo(x):
     return fibo(x - 1) + fibo(x - 2)
 print(fibo(4))
 """
-# 호출되는 함수 확인
+
+"""
+# 호출되는 함수 확인 -> Top-down
 d = [0] * 100
 
 def pibo(x):
@@ -19,3 +21,16 @@ def pibo(x):
     print(d)
     return d[x]
 pibo(6)
+"""
+
+# 피보나치 수열 소스코드(반복적) -> bottom-up 
+d = [0] * 100
+
+# 첫 번째 피보나치 수와 두 번째 피보나치 수는 1이다.
+d[1] = 1
+d[2] = 1
+
+for i in range(3, 100):
+    d[i] = d[i - 2] + d[i - 1]
+
+print(d[4])
