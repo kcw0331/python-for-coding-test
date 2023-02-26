35f1749603390989406cc406100eb3531cec3302
Changwoo Kim <kcw0331@cau.ac.kr>
2023-02-26 18:38:23 +0900

    2023년2월26일-백준(2740번)-행렬곱셈(분할정복)

---
+++ baekjoo/2740.py
@@ -0,0 +1,17 @@
+# 백준(2740번) - 행렬 곱셈(분할정복)
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+A = [list(map(int, input().split())) for i in range(n)]
+m, k = map(int, input().split())
+B = [list(map(int, input().split())) for i in range(m)]
+
+for i in range(n):
+  result = []
+  for l in range(k):
+    a = 0
+    for j in range(m):
+      a += A[i][j] * B[j][l]
+    result.append(a)
+  print(*result)
