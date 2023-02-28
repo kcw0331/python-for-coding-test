8b80ad05ccf5c1349d38ca960d1cf2a3d958f96a
Changwoo Kim <kcw0331@cau.ac.kr>
2023-02-28 23:15:17 +0900

    2023년2월28일-백준(1021번)-회전하는큐(큐와덱)

---
+++ baekjoo/1021.py
@@ -0,0 +1,25 @@
+# 백준(1021번) - 회전하는 큐(큐와 덱)
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+idxs = list(map(int, input().split()))
+dq = deque([i for i in range(1, n+1)])
+
+count = 0
+for idx in idxs:
+  while True:
+    if dq[0] == idx:
+      dq.popleft()
+      break
+    else:
+      if dq.index(idx) < len(dq) / 2:
+        while dq[0] != idx:
+          dq.append(dq.popleft())
+          count += 1
+      else:
+        while dq[0] != idx:
+          dq.appendleft(dq.pop())
+          count += 1
+print(count)
