6e57996e1dcbac7800930488f350ebaa93c0fb65
Changwoo Kim <kcw0331@cau.ac.kr>
2023-03-04 23:07:47 +0900

    2023년3월4일-백준(1991번)-트리(트리순회)추가내용정리

--- baekjoo/1991.py
+++ baekjoo/1991.py
@@ -1,30 +1,48 @@
 # 백준(1991번) - 트리(트리순회)
+"""
+입력
+7
+A B C
+B D .
+C E F
+E . .
+F . G
+D . .
+G . .
+
+출력
+ABDCEFG
+DBAECFG
+DBEGFCA
+"""
 n = int(input())
 inputs = []
 for _ in range(n):
   inputs.append(input().split())
 
-class Node():
+class Node(): # 클래스 생성
   def __init__(self, item, left, right):
-    self.item = item
+    # self를 사용해서 초기화
+    self.item = item 
     self.left = left 
     self.right = right
 
-def preorder(node):
-  print(node.item, end='')
+def preorder(node): # 전위 순회
+  print(node.item, end='') # 전위 순회라서 처음에 루트
+  # if문을 두개 걸어줘서 재귀를 다하고 다은거를 재귀를 해준다.
   if node.left != '.':
-    preorder(tree[node.left])
+    preorder(tree[node.left]) # 왼쪽이 있다면 왼쪽
   if node.right != '.':
-    preorder(tree[node.right])
+    preorder(tree[node.right]) # 오른쪽이 있다면 오른쪽
 
-def inorder(node):
+def inorder(node): # 중위 순회
   if node.left != '.':
     inorder(tree[node.left])
   print(node.item, end = '')
   if node.right != '.':
     inorder(tree[node.right])
 
-def postorder(node):
+def postorder(node): # 후위 순회
   if node.left != '.':
     postorder(tree[node.left])
   if node.right != '.':
@@ -32,7 +50,7 @@ def postorder(node):
   print(node.item, end = '')
 
 tree = {}
-for item, left, right in inputs:
+for item, left, right in inputs: # inputs 리스트를 for문을 사용해서 하나씩 끄집어냄
   tree[item] = Node(item, left, right)
 
 preorder(tree['A'])
