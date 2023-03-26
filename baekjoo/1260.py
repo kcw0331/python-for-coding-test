ef6598e91c4a5b66792a3ca4a86b88236e45638c
kcw0331 <kcw0331@cau.ac.kr>
2023-03-26 21:42:31 +0900

    2023년3월26일-백준(1260번)-DFS와BFS

--- baekjoo/1260.py
+++ baekjoo/1260.py
@@ -1,11 +1,22 @@
 # 백준(1260번) - DFS와BFS
+from collections import deque
 def dfs(v):
   visited[v] = True
   print(v, end = ' ')
   for i in graph[v]:
     if not visited[i]:
       dfs(i)
-      
+def bfs(v):
+  queue = deque([v])
+  visited[v] = True
+  while queue:
+    V = queue.popleft()
+    print(V, end = ' ')
+    for i in graph[V]:
+       if not visited[i]:
+         queue.append(i)
+         visited[i] = True
+
 n, m, v = map(int, input().split())
 
 visited = [False] * (n + 1)
