6d767c71751b89f2d88b4f1195fe47fa09b50c19
kcw0331 <kcw0331@cau.ac.kr>
2023-03-31 17:02:34 +0900

    2023년3월31일-백준(2178번)-BFS(미로탐색)

---
+++ baekjoo/2178.py
@@ -0,0 +1,27 @@
+# BFS
+# 백준(미로 탐색) - BFS
+import sys
+from collections import deque
+input = sys.stdin.readline
+n, m = map(int, input().split())
+graph = []
+for _ in range(n):
+  graph.append(list(map(int, input().rstrip())))
+
+dx = [-1, 1, 0, 0] # 상, 하
+dy = [0, 0, -1, 1] # 좌, 우
+
+# bfs 함수
+def bfs(x, y):
+  queue = deque()
+  queue.append((x, y))
+  while queue:
+    x, y = queue.popleft()
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+        queue.append((nx, ny))
+        graph[nx][ny] = graph[x][y] + 1
+  return graph[n - 1][m - 1]
+print(bfs(0, 0))
