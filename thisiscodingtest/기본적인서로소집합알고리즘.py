6ac73f184aa152868c39ba667e6b416ed9c7d64f
kcw0331 <kcw0331@cau.ac.kr>
2023-04-02 18:21:21 +0900

    2023년4월2일-경로압축기법소스코드

--- thisiscodingtest/기본적인서로소집합알고리즘.py
+++ thisiscodingtest/기본적인서로소집합알고리즘.py
@@ -1,9 +1,15 @@
 # 특정 원소가 속한 집합을 찾기
+# def find_parent(parent, x):
+#     # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+#     if parent[x] != x:
+#         return find_parent(parent, parent[x])
+#     return x
+
+# 경로 압축 기법 소스코드
 def find_parent(parent, x):
-    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
     if parent[x] != x:
-        return find_parent(parent, parent[x])
-    return x
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
 
 # 두 원소가 속한 집합을 합치기
 def union_parent(parent, a, b):
