0292307f1cd778f56d7762c037a70eace86d1247
kcw0331 <kcw0331@cau.ac.kr>
2023-04-08 14:07:24 +0900

    2023년4월8일-프로그래머스(해시-베스트앨범)

--- programmers-coding/베스트앨범.py
+++ programmers-coding/베스트앨범.py
@@ -13,5 +13,14 @@ def solution(genres, plays):
         for song in _dict[i]:
             plays_sum += song[0]
         _dict_rank[i] = plays_sum
-    _dict_rank = sorted(_dict_rnak.items(), key = lambda x : x[1], reverse=True)
-    print(_dict_rank)
+    _dict_rank = sorted(_dict_rank.items(), key = lambda x : x[1], reverse=True)
+    # plays를 정렬 시켜서 만들어준다.
+    for j in _dict_rank:
+        song_rank = sorted(_dict[j[0]], key = lambda x : (-x[0], x[1]))
+        best_two = 0
+        for song in song_rank:
+            answer.append(song[1])
+            best_two += 1
+            if best_two == 2:
+                break
+    return answer
