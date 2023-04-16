046290e69d855364d41bf94db8e4e6257ac4cfbb
kcw0331 <kcw0331@cau.ac.kr>
2023-04-16 21:41:38 +0900

    2023년4월16일-프로그래머스(완전탐색-소수찾기)

--- programmers-coding/소수찾기.py
+++ programmers-coding/소수찾기.py
@@ -20,3 +20,27 @@ def solution(numbers):
             answer += 1
 
     return answer
+
+# ========================================================================
+# 2023년 4월 16일 문제를 다시 풀어봄.
+from itertools import permutations
+def primenumber(x):
+    if x < 2:
+        return False
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
+
+def solution(numbers):
+    answer = 0
+    result = []
+    for number in range(1, len(numbers)+1):
+        first = list(set(map(''.join, permutations(numbers, number))))
+        result.append(first)
+    unduplicated_numbers = list(set(map(int, sum(result, []))))
+    
+    for i in unduplicated_numbers:
+        if primenumber(i) == True:
+            answer += 1
+    return answer
