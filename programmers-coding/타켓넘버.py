bed31fcf0d016befcc44865973b74c470688fc9b
kcw0331 <kcw0331@cau.ac.kr>
2023-04-26 22:08:14 +0900

    2023년4월26일-프로그래머스(dfs-타겟넘버)

--- programmers-coding/타켓넘버.py
+++ programmers-coding/타켓넘버.py
@@ -6,3 +6,19 @@ def solution(numbers, target):
     number_list = list(map(sum, product(*number)))
     # print(number_list)
     return number_list.count(target)
+
+# dfs 방식으로 문제를 푸는 방식이다.(다른 사람이 푼거를 dfs를 공부하기 위해 적어보았다.)
+def solution(numbers, target):
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result+numbers[idx])
+    dfs(0,0)
+    return answer
