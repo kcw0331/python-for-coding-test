81a04a4ec41ac179a439ac52dba3306e00b25bdb
kcw0331 <kcw0331@cau.ac.kr>
2023-04-27 23:30:26 +0900

    2023년4월27일-프로그래머스(DFS,BFS-타겟넘버)

--- programmers-coding/타켓넘버.py
+++ programmers-coding/타켓넘버.py
@@ -19,6 +19,25 @@ def solution(numbers, target):
             return
         else:
             dfs(idx+1, result+numbers[idx])
-            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
     dfs(0,0)
+    return answer
+
+# bfs 방식으로 문제를 푸는 방식이다.
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    n = len(numbers)
+    queue.append([numbers[0], 0])
+    queue.append([-1*numbers[0],0])
+    while queue:
+        temp, idx = queue.popleft()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
     return answer
