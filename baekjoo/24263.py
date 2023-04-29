e7744b9261dbba8bdaec932011b0b2c59ab61da3
kcw0331 <kcw0331@cau.ac.kr>
2023-04-29 22:20:57 +0900

    2023년4월29일-백준(시간복잡도-알고리즘의수행시간2)

---
+++ baekjoo/24263.py
@@ -0,0 +1,13 @@
+# 백준 - 알고리즘 수업 - 알고리즘의 수행 시간 2
+"""
+MenOfPassion(A[], n) {
+    sum <- 0;
+    for i <- 1 to n
+        sum <- sum + A[i]; # 코드1
+    return sum;
+}
+"""
+
+n = int(input())
+print(n)
+print(1)
