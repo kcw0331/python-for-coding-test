a42506fb3f9b32cbe6fe12085e604d5a415cf141
kcw0331 <kcw0331@cau.ac.kr>
2023-04-30 23:05:48 +0900

    2023년4월30일-프로그래머스(DFS/BFS-네트워크)

--- programmers-coding/네트워크.py
+++ programmers-coding/네트워크.py
@@ -12,12 +12,15 @@ def solution(n, computers):
     for com in range(n):
         if visited[com] == False:
             DFS(n, computers, com, visited)
+            # DFS의 함수가 끝나게 되면 answer에 1을 더해준다.
             answer += 1
     return answer
 
 def DFS(n, computers, com, visited):
     visited[com] = True
     for connect in range(n):
+        # if문이 만족이 된다면, DFS가 계속 이루어지게 된다
+        # if문이 계속이루어 지게 된다면, 서로 연결되어 있다고 생각해도 될거 같다
         if connect != com and computers[com][connect] == 1:
             if visited[connect] == False:
                 DFS(n, computers, connect, visited)
