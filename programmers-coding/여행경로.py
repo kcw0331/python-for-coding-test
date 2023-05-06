53389e39e8490d710c85e1df7dcd42d5e8f470d4
kcw0331 <kcw0331@cau.ac.kr>
2023-05-06 13:06:49 +0900

    2023년5월6일-프로그래머스(DFS/BFS-여행경로)

--- programmers-coding/여행경로.py
+++ programmers-coding/여행경로.py
@@ -3,7 +3,7 @@ def solution(tickets):
     answer = []
     # result = [False for _ in range(len(tickets))]
     queue = deque()
-    # 출발 공항, 방문하는 공항 경로, 방문 경로를 저장
+    # 출발 공항, 방문하는 공항 경로, 방문 경로를 저장(튜플을 사용)
     queue.append(("ICN",["ICN"],[]))
     
     while queue: # 큐에 값이 있을 때 까지만 while문 실행
@@ -11,7 +11,7 @@ def solution(tickets):
         airport, path, used = queue.popleft()
         # 전체 방문 경로와 티켓의 길이가 같다면, answer에 삽입
         if len(used) == len(tickets):
-            answer.append(path)
+            answer.append(path) # answer에 path를 삽입
         # tickets을 for문을 사용
         for idx, ticket in enumerate(tickets):
             # ticket의 첫번째 변수와 airport가 같고 used에 없는 값이 라면 if을 실행
@@ -19,5 +19,5 @@ def solution(tickets):
                 # 큐에 ticket의 두번째 값을 삽입
                 queue.append((ticket[1], path + [ticket[1]], used + [idx]))
                 # print(path)
-    answer.sort()
-    return answer[0]
+    answer.sort() # 이후 answer를 오름차순으로 정렬
+    return answer[0] # answer의 첫번째 변수를 출력
