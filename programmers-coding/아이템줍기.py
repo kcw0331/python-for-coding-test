9f23740a2059c712be026207afd0d11e6066648b
kcw0331 <kcw0331@cau.ac.kr>
2023-05-07 23:22:00 +0900

    2023년5월7일-프로그래머스(DFS/BFS-아이템줍기)

---
+++ programmers-coding/아이템줍기.py
@@ -0,0 +1,8 @@
+"""
+지형을 나타내는 직사각형이 담긴 2차원 배열: rectangle
+초기 캐릭터의 위치: characterX, characterY
+아이템의 위치: itemX, itemY
+"""
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    answer = 0
+    return answer
