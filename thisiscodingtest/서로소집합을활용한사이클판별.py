9f119b84665ee6cff546cbb10a334aa093e502ca
kcw0331 <kcw0331@cau.ac.kr>
2023-05-17 23:09:18 +0900

    2023년5월17일-그래프이론(서로소집합을활용한사이클판별)

---
+++ thisiscodingtest/서로소집합을활용한사이클판별.py
@@ -0,0 +1,35 @@
+# 특정 원소가 속한 잡합을 찾기
+def find_parent(parent, x):
+    # 루투 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().split()) # v: 노드의 개수, e: 간선
+parent = [0] * (v + 1) # [0, 0, 0, 0]
+
+for i in range(1, v + 1):
+    parent[i] = i
+
+cycle = False
+
+for i in range(e):
+    a, b = map(int, input().split())
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    else:
+        union_parent(parent, a, b)
+
+if cycle:
+    print("사이클이 발생했습니다.")
+else:
+    print("사이클이 발생하지 않았습니다.")
