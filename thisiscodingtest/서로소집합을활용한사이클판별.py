f125c464fb79f44d3ba2e92f9d24c1d0795ca6de
kcw0331 <kcw0331@cau.ac.kr>
2023-05-18 22:23:20 +0900

    2023년5월18일-그래프이론(서로소집합을활용한사이클판단)

--- thisiscodingtest/서로소집합을활용한사이클판별.py
+++ thisiscodingtest/서로소집합을활용한사이클판별.py
@@ -1,3 +1,10 @@
+"""
+입력예시:
+3 3
+1 2
+1 3
+2 3
+"""
 # 특정 원소가 속한 잡합을 찾기
 def find_parent(parent, x):
     # 루투 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
@@ -23,7 +30,7 @@ cycle = False
 
 for i in range(e):
     a, b = map(int, input().split())
-    if find_parent(parent, a) == find_parent(parent, b):
+    if find_parent(parent, a) == find_parent(parent, b): # 에) 1 == 2가 같지 않아서 else 실행
         cycle = True
         break
     else:
@@ -33,3 +40,10 @@ if cycle:
     print("사이클이 발생했습니다.")
 else:
     print("사이클이 발생하지 않았습니다.")
+
+"""
+예)
+[0, 1, 1, 3]
+[0, 1, 1, 1]
+-> a = 2, b = 3을 실행할 때, 1 == 1이 같기 때문에 cycle = True가 되고 break가 된다.
+"""
