4478948a46ced2cfe29a718a023fbcc5a5a00246
kcw0331 <kcw0331@cau.ac.kr>
2023-05-25 23:18:50 +0900

    2023년5월25일-그래프이론(위상정렬)

--- thisiscodingtest/위상정렬.py
+++ thisiscodingtest/위상정렬.py
@@ -1,13 +1,13 @@
 from collections import deque
 
 v, e = map(int, input().split())
-indegree = [0] * (v + 1)
-graph = [[] for i in range(v + 1)]
+indegree = [0] * (v + 1) # 모든 노드에 대한 진입차수를 [0, 0, 0, 0, 0, 0, 0, 0]로 0으로 초기화 한다
+graph = [[] for i in range(v + 1)] # [[], [], [], [], [], [], [], []]
 
 for _ in range(e):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    indegree[b] += 1
+    graph[a].append(b) # [[], [2, 5], [3, 6], [4], [7], [6], [4], []]
+    indegree[b] += 1 # [0, 0, 1, 1, 2, 1, 2, 1]
 print(indegree)
 print(graph)
 # ex) indegree => [0, 0, 1, 1, 2, 1, 2, 1]
@@ -20,18 +20,18 @@ def topology_sort():
 
     for i in range(1, v + 1):
         if indegree[i] == 0:
-            q.append(i)
+            q.append(i) # 0이라면, 큐에 집어넣어 준다.
 
-# 큐 빌 때까지 반복
-while q:
-    now = q.popleft()
-    result.append(now)
-    for i in graph[now]:
-        indegree[i] -= 1
-        if indegree[i] == 0:
-            q.append(i)
+    # 큐 빌 때까지 반복
+    while q:
+        now = q.popleft()
+        result.append(now)
+        for i in graph[now]:
+            indegree[i] -= 1
+            if indegree[i] == 0: 
+                q.append(i) # 0이라면 큐에 넣어주고 아니라면 넘어간다.
 
-for i in result:
-    print(i, end=' ')
+    for i in result:
+        print(i, end=' ')
 
 topology_sort()
