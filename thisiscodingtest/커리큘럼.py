b647180e0a0cfe821b949e665b7965a14c5a4196
kcw0331 <kcw0331@cau.ac.kr>
2023-05-31 22:41:13 +0900

    2023년5월31일-그래프이론(커리큘럼_위상정렬)

--- thisiscodingtest/커리큘럼.py
+++ thisiscodingtest/커리큘럼.py
@@ -1,2 +1,14 @@
-# 입력 조건
+# 입력 조건 - 위상 정렬을 사용한다.
 # - 첫째 줄에 동빈이가 듣고자 하는 강의의 수 
+
+n = int(input())
+indegree = [0] * (n + 1)
+graph = [[] for _ in range(n + 1)]
+time = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    data = list(map(int, input().split()))
+    time[i] = data[0]
+    for x in data[1:-1]:
+        indegree[i] += 1
+        graph[x].append(i)
