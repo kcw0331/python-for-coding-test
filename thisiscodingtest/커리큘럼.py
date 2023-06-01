95393a1de736cf51175e8ac09c10b129a5a5b6e5
kcw0331 <kcw0331@cau.ac.kr>
2023-06-01 22:36:36 +0900

    2023년6월1일-그래프이론(위상정렬_커리큘럼)

--- thisiscodingtest/커리큘럼.py
+++ thisiscodingtest/커리큘럼.py
@@ -1,5 +1,7 @@
 # 입력 조건 - 위상 정렬을 사용한다.
 # - 첫째 줄에 동빈이가 듣고자 하는 강의의 수 
+from collections import deque
+import copy
 
 n = int(input())
 indegree = [0] * (n + 1)
@@ -12,3 +14,22 @@ for i in range(1, n + 1):
     for x in data[1:-1]:
         indegree[i] += 1
         graph[x].append(i)
+
+def topology_sort():
+    result = copy.deepcopy(time)
+    q = deque()
+
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            result[i] = max(result[i], result[now] + time[i])
+            indegree -= 1
+            if indegree[i] == 0:
+                q.append(i)
+        for i in range(1, n + 1):
+            print(result[i])
+topology_sort()
