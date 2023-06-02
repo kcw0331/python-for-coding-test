7b1f72cdaca15184c33722eaba6ac86ee68ad19a
kcw0331 <kcw0331@cau.ac.kr>
2023-06-02 22:52:48 +0900

    2023년6월2일-그래프이론(위상정렬_커리큘럼)

--- thisiscodingtest/커리큘럼.py
+++ thisiscodingtest/커리큘럼.py
@@ -3,14 +3,16 @@
 from collections import deque
 import copy
 
-n = int(input())
-indegree = [0] * (n + 1)
-graph = [[] for _ in range(n + 1)]
-time = [0] * (n + 1)
+n = int(input()) # 노드의 개수 입력받기
+indegree = [0] * (n + 1) # 모든 노드에 대한 진입차수는 0으로 초기화
+graph = [[] for _ in range(n + 1)] # 각 노드에 연결된 간선 정보를 담기 위한 연결 리스트(그래프) 초기화
+time = [0] * (n + 1) # 각 강의 시간을 0으로 초기화
 
+# 방향 그래프의 모든 간선 정보를 입력받기
 for i in range(1, n + 1):
     data = list(map(int, input().split()))
     time[i] = data[0]
+    # print(data[1:-1])
     for x in data[1:-1]:
         indegree[i] += 1
         graph[x].append(i)
@@ -32,4 +34,4 @@ def topology_sort():
                 q.append(i)
         for i in range(1, n + 1):
             print(result[i])
-topology_sort()
+# topology_sort()
